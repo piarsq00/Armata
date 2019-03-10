c424c38b072d49f725a12157b4825eaa2be965cd
Arkadiusz Dobrzański <arkadiusz.dobrzanski@o2.pl>
2019-03-10 17:29:47 +0100

    Zatwierdzam

---
+++ Armata/Armata.cpp
@@ -0,0 +1,29 @@
+﻿// Armata.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+
+class Armata {
+protected:
+	float kat = 0;
+	float Vp = 0;
+	float m = 0;
+	float g;
+public:
+	void licz_zasieg();
+	void licz_wysokosc();
+	void czas_lotu();
+	Armata();
+	void wstaw_dane(float kat, float Vp, float m, float g);
+	float zasieg();
+
+
+};
+
+
+int main()
+{
+	return 0;
+}
+
